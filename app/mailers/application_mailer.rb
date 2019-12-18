a6202cf512b2b4b7469624909edcc4c9435a01d8
tibbs001 <sheri.tibbs@duke.edu>
2019-12-18 09:59:17 -0500

    Remove references to tibbs.  Use env vars exclusively.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,11 +3,7 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def self.admin_addresses
-    if Rails.env.capitalize == 'Production'
-      ['sheri.tibbs@duke.edu', 'ctti-aact@duke.edu']
-    else
-     ['sheri.tibbs@duke.edu', 'sheri.tibbs@gmail.com']
-    end
+    [ENV['AACT_ADMIN_EMAILS']]
   end
 
 end
